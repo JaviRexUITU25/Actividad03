6f62d5de7ad142fa6e27ab1c05b0f7e3cb0b6811
fabri <fabritzioalejandrol@gmail.com>
2025-07-16 18:18:44 -0600

    Menu de conversiones

--- Actividad 3.py
+++ Actividad 3.py
@@ -6,3 +6,10 @@ while True:
     print("2. Conversión de temperatura.")
     print("3. Salir del programa.")
     opcion= int(input("Elija una de las opciones:"))
+    if opcion == 1:
+        while True:
+            print("Mené de conversiones")
+            print("1. Conversión de metros a centimetros.")
+            print("2. Conversión de centimetros a metros.")
+            print("3. Conversión de kilogramos a libras.")
+            print("4. Conversión de libras a kilogramos. ")
