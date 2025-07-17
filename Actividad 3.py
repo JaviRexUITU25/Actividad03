6047261225cdb2d6329c9a1be1be7efa7495e46a
fabri <fabritzioalejandrol@gmail.com>
2025-07-16 18:13:03 -0600

    Menu principal

--- Actividad 3.py
+++ Actividad 3.py
@@ -1,1 +1,8 @@
-#Ideas:
+#Fabritzio Lopez
+#Javier Huertas
+while True:
+    print("Menú Principal")
+    print("1. Conversión de unidades.")
+    print("2. Conversión de temperatura.")
+    print("3. Salir del programa.")
+    opcion= int(input("Elija una de las opciones:"))
