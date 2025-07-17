e39fc3fae5f6dd263ba8b22b4d647f8c4f2f8162
Javier Huertas <javiandre2007@gmail.com>
2025-07-17 08:50:21 -0600

    FINAL VERSION MAIN in class

--- Actividad 3.py
+++ Actividad 3.py
@@ -5,8 +5,8 @@ while True:
     print("1. Conversión de unidades.")
     print("2. Conversión de temperatura.")
     print("3. Salir del programa.")
-    option= int(input("-----Elija una de las opciones------: "))
-    if option == 1:
+    option= input("-----Elija una de las opciones------: ")
+    if option == "1":
         while True:
             print("---------Menú de conversiones---------")
             print("1. Conversión de metros a centimetros.\n")
@@ -14,51 +14,51 @@ while True:
             print("3. Conversión de kilogramos a libras.\n")
             print("4. Conversión de libras a kilogramos.\n ")
             print("5. Volver al menu principal.")
-            opc= int((input("------Ingrese una opción de conversiones------: ")))
-            if opc == 1:
-                metros= int(input("Ingrese la cantidad de metros que quiere convertir a centimetros: "))
+            opc= input("------Ingrese una opción de conversiones------: ")
+            if opc == "1":
+                metros= input("Ingrese la cantidad de metros que quiere convertir a centimetros: ")
                 cm= metros * 100
                 print(f"La cantidad de {metros} metros equivale a {cm} centimetros.")
-            elif opc == 2:
+            elif opc == "2":
                 centimetros= int(input("Ingrese la cantidad de centimetros que quiere convertir a metros: "))
                 m= centimetros / 100
                 print(f"La cantidad de {centimetros} centimetros equivale a {m} metros.")
-            elif opc == 3:
+            elif opc == "3":
                 kilogramos= int(input("Ingrese la cantidad de kilometros que quiere convertir a libras: "))
                 libras= kilogramos * 2.2046
                 print(f"La cantidad de {kilogramos} kg equivale a {libras} lbs.")
-            elif opc == 4:
+            elif opc == "4":
                 lb = int(input("Ingrese la cantidad de libras que quiere convertir a kilogramos: "))
                 kg= lb / 2.2046
                 print(f"La cantidad de {lb} lbs equivale a {kg} kgs.")
-            elif opc == 5:
+            elif opc == "5":
                 break
             else:
                 print("No existe esta opcion, intentalo de nuevo: ")
     #option= int(input("-----Elija una de las opciones------: "))
-    if option == 2:
+    if option == "2":
         while True:
             print("---------Conversion de temperatura---------")
             print(("1. Celcius a Fahrenheit\n"
                     "2. Fahrenheit a Celcius\n"
                     "3. Volver al menú principal."))
-            opc = int((input("-----Ingrese la opción que desea-----: ")))
-            if opc == 1:
+            opc = input("-----Ingrese la opción que desea-----: ")
+            if opc == "1":
                 Cgrades = int(input("Ingrese la cantidad de grados Celcius: "))
                 fah = 9/5 * Cgrades + 32
                 print(f"La cantidad en grados fahrenheit es de: {fah} grados")
-            elif opc == 2:
+            elif opc == "2":
                 fahrenheit = int(input("Ingrese la cantidad de grados Fahrenheit: "))
                 cel = fahrenheit - 32 / 1.8
                 print(f"La cantidad en grados Celcis es de: {cel} grados")
-            elif opc == 3:
+            elif opc == "3":
                 break
             else:
                 print("No existe esta opcion, intentalo de nuevo: ")
     #option= int(input("-----Elija una de las opciones------: "))
-    if option == 3:
+    if option == "3":
         while True:
-            if option != 3:
+            if option != "3":
                 print("No existe esta opcion, intentalo de nuevo: ")
             else:
                     print("¡Gracias por usar el programa!")
